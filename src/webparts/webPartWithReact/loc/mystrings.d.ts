d94bb24d4bad9badb979af6bdfc9e01be54161f9
heesungjang <heesungj7@gmail.com>
2023-02-06 10:29:13 +1000

    init

---
+++ src/webparts/webPartWithReact/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface IWebPartWithReactWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'WebPartWithReactWebPartStrings' {
+  const strings: IWebPartWithReactWebPartStrings;
+  export = strings;
+}
